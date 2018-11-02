79f506820fcae940b248d4aca417e4647822a7bb
Kara Thrash <dvdonato@gmail.com>
2018-11-02 17:16:27 -0400

    sd

--- mine/fourbitadder.v
+++ mine/fourbitadder.v
@@ -4,10 +4,10 @@ output s,cout;
 wire w1, w2, w3, w4, s, cout;
 reg a, b, c;
 
-xor g1(w1, a, b),
+xor g1(w1, a, b);
 xor g2(s, w1, c);
- and g3(w2, c, b),
- and g4(w3, c, a),
+ and g3(w2, c, b);
+ and g4(w3, c, a);
  and g5(w4, a, b);
 or g6(cout, w2, w3, w4);
 
