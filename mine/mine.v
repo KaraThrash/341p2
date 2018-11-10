74e94ce188781d1bd3777a5721a55f9d1af4887d
starbuck <dvdonato@buffalo.edu>
2018-11-09 19:35:11 -0500

    adderand sub works

--- mine/mine.v
+++ mine/mine.v
@@ -3,25 +3,64 @@
 module oneadder(a,b,c,s,cout);
 input a,b,c;
 output s,cout;
-wire w1, w2, w3, w4, s, cout;
-reg a, b, c;
+wire w1, w2, w3, w4, s;
 
-xor g1(w1, a, b),
+
+xor g1(w1, a, b);
 xor g2(s, w1, c);
- and g3(w2, c, b),
- and g4(w3, c, a),
+ and g3(w2, c, b);
+ and g4(w3, c, a);
  and g5(w4, a, b);
 or g6(cout, w2, w3, w4);
 endmodule
 
-module mine(x,y,s,cout,cin);
-input [3:0] x,y;
+module fulladder(x,y,s,cout,cin);
+input [3:0] y;
+input [3:0] x;
 output [3:0] s;
 input cin;
 output cout;
 wire c[3:0];
+
  oneadder f0 (x[0],y[0],cin,s[0],c[0]);
  oneadder f1 (x[1],y[1],c[0],s[1],c[1]);
  oneadder f2 (x[2],y[2],c[1],s[2],c[2]);
  oneadder f3 (x[3],y[3],c[2],s[3],cout);
+endmodule
+
+module negate(x,y,s,cout,cin);
+input [3:0] y;
+input [3:0] x;
+output [3:0] s;
+input cin;
+output cout;
+wire one,bw[3:0],c[3:0];
+assign{one} = 1;
+not(bw[0],y[0]);
+not(bw[1],y[1]);
+not(bw[2],y[2]);
+not(bw[3],y[3]);
+ oneadder f0 (x[0],bw[0],one,s[0],c[0]);
+ oneadder f1 (x[1],bw[1],c[0],s[1],c[1]);
+ oneadder f2 (x[2],bw[2],c[1],s[2],c[2]);
+ oneadder f3 (x[3],bw[3],c[2],s[3],cout);
+
+
+endmodule
+
+module mine(x,y,s,cout,cin);
+input [3:0] y;
+input [3:0] x;
+output [3:0] s;
+wire extra;
+wire [3:0] yneg;
+input cin;
+output cout;
+wire c[3:0];
+negate negfun(x,y,yneg,cout,cin);
+ oneadder f0 (x[0],yneg[0],cin,s[0],c[0]);
+
+ oneadder f1 (x[1],yneg[1],c[0],s[1],c[1]);
+ oneadder f2 (x[2],yneg[2],c[1],s[2],c[2]);
+ oneadder f3 (x[3],yneg[3],c[2],s[3],cout);
 endmodule
--- mine/mine_tb.v
+++ mine/mine_tb.v
@@ -4,21 +4,29 @@
 
 module mine_tb;
 
+wire[3:0] aa;
+wire[3:0] bb;
+wire[3:0] runsum;
+wire[3:0] sum;
+wire c_out;
 wire A, B, C, D, E;
 integer k=0;
 
-assign {C,D} = k;
-assign {A,B} = 1;
-mine the_circuit(A, B, C, D, E);
+assign {aa} = k;
+assign {bb} = 0;
+assign {E} = 0;
+assign runsum = sum;
+mine the_circuit(bb,aa, sum, c_out, E);
 
    initial begin
-
+	
       $dumpfile("mine.vcd");
       $dumpvars(0, mine_tb);
-
+$display("first %b", aa);
       for (k=0; k<8; k=k+1)
-        #10 $display("from mine done testing case %d", k);
-         $monitor ("req0=%b,req1=%b,gnt0=%b,gnt1=%b", A, B, C, D);
+        #10 $display("from mine doe %b as welel as %b", runsum,bb);
+       $display("other %b", runsum);
+
       $finish;
 
    end
